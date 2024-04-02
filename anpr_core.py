6682464d4c56ee6b5c481b0f01a41b1d5926154c
Rosso <brandon.rosso@xpertdox.com>
2024-04-01 22:15:47 -0500

    working basic anpr

--- anpr_core.py
+++ anpr_core.py
@@ -1,16 +1,18 @@
 import os
 import cv2
+import time
 import logging
 import pytesseract
 import xml.etree.ElementTree as ET
 
-output_folder = "C:\\Users\\Brandon\\personal_code\\CARL\\OUTPUT\\"
+OUTPUT_FOLDER = "C:\\Users\\Brandon\\personal_code\\CARL\\OUTPUT\\"
+INPUT_IMAGES = "C:\\Users\\Brandon\\personal_code\\CARL\\test\\test_group_1\\"
+SHOW_IMAGES = True
 
-
-def load_image():
+def load_image(file2load):
     try:
         # Load the image
-        image_path = "C:\\Users\\Brandon\\personal_code\\CARL\\old\\practice1\\test_group1\\Cars111.png"
+        image_path = f"{INPUT_IMAGES}{file2load}"
         image = cv2.imread(image_path)
 
         # Check if the image is loaded successfully and has the correct number of channels (BGR with 3 channels)
@@ -26,7 +28,6 @@ def load_image():
         return image
 
 
-
 def preprocess_image(_image):
     try:
         # Convert the image to grayscale
@@ -35,17 +36,32 @@ def preprocess_image(_image):
         # Apply Gaussian blur to reduce noise
         blurred_image = cv2.GaussianBlur(gray_image, (5, 5), 0)
 
-        # Display the original and preprocessed images (optional)
-        cv2.imshow("Original Image", _image)
-        cv2.imshow("Gray Scaled Image", gray_image)
-        cv2.imshow("Blurred Image (Noise Reduction Technique)", blurred_image)
-        cv2.waitKey(0)
-        cv2.destroyAllWindows()
+        if SHOW_IMAGES:
+            # Display the original and preprocessed images (optional)
+            cv2.imshow("Original Image", _image)
+            cv2.imshow("Gray Scaled Image", gray_image)
+            cv2.imshow("Blurred Image (Noise Reduction Technique)", blurred_image)
+            cv2.waitKey(0)
+            cv2.destroyAllWindows()
     except Exception:
         logging.exception("IMAGE PREPROCESSING FAILURE", exc_info=True)
         return None
     else:
         return blurred_image
+
+
+def draw_roi(image):
+    """
+    Allows the user to draw a rectangle on the image to specify the region of interest (ROI).
+    Returns the ROI rectangle coordinates (x, y, width, height).
+    """
+    # Display the image and allow the user to draw a rectangle
+    clone = image.copy()
+    roi_rectangle = cv2.selectROI("Select ROI", clone, fromCenter=False, showCrosshair=True)
+    cv2.destroyWindow("Select ROI")  # Close the window after selection
+
+    # Return the ROI rectangle coordinates
+    return roi_rectangle
 
 
 def detect_and_localize(_preproc_image):
@@ -63,10 +79,11 @@ def detect_and_localize(_preproc_image):
         # Draw contours on the original image (optional)
         image_with_contours = cv2.drawContours(_preproc_image.copy(), contours, -1, (0, 255, 0), 2)
 
-        # Display the original image with contours (optional)
-        cv2.imshow("Image with Contours", image_with_contours)
-        cv2.waitKey(0)
-        cv2.destroyAllWindows()
+        if SHOW_IMAGES:
+            # Display the original image with contours (optional)
+            cv2.imshow("Image with Contours", image_with_contours)
+            cv2.waitKey(0)
+            cv2.destroyAllWindows()
     except Exception:
         logging.exception("DETECT & LOCALIZE FAILURE", exc_info=True)
         return None
@@ -74,7 +91,7 @@ def detect_and_localize(_preproc_image):
         return contours
 
 
-def extract_lp_region(_contours, _preprocessed_image):
+def extract_lp_region(_contours, _preprocessed_image, epsilon=0.01):
     try:
         # Sort contours by area in descending order
         contours = sorted(_contours, key=cv2.contourArea, reverse=True)
@@ -83,7 +100,7 @@ def extract_lp_region(_contours, _preprocessed_image):
         license_plate_contour = None
         for contour in contours:
             perimeter = cv2.arcLength(contour, True)
-            approx = cv2.approxPolyDP(contour, 0.02 * perimeter, True)
+            approx = cv2.approxPolyDP(contour, epsilon * perimeter, True)  # Adjust epsilon value here
             if len(approx) == 4:  # Assuming license plate contours are rectangular
                 license_plate_contour = approx
                 break
@@ -92,10 +109,11 @@ def extract_lp_region(_contours, _preprocessed_image):
         image_with_license_plate = cv2.drawContours(_preprocessed_image.copy(), [license_plate_contour], -1, (0, 255, 0),
                                                     2)
 
-        # Display the original image with license plate contour (optional)
-        cv2.imshow("Image with License Plate Contour", image_with_license_plate)
-        cv2.waitKey(0)
-        cv2.destroyAllWindows()
+        if SHOW_IMAGES:
+            # Display the original image with license plate contour (optional)
+            cv2.imshow("Image with License Plate Contour", image_with_license_plate)
+            cv2.waitKey(0)
+            cv2.destroyAllWindows()
     except Exception:
         logging.exception("FAILURE TO EXTRACT LICENSE PLATE REGION", exc_info=True)
         return None
@@ -126,10 +144,11 @@ def further_processing(_license_plate_contour, _preprocessed_image):
         # Apply binarization to the license plate region
         _, binary_license_plate = cv2.threshold(gray_license_plate, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
 
-        # Display the segmented license plate (optional)
-        cv2.imshow("Segmented License Plate", binary_license_plate)
-        cv2.waitKey(0)
-        cv2.destroyAllWindows()
+        if SHOW_IMAGES:
+            # Display the segmented license plate (optional)
+            cv2.imshow("Segmented License Plate", binary_license_plate)
+            cv2.waitKey(0)
+            cv2.destroyAllWindows()
 
         # Return the segmented license plate for further use or analysis
         return binary_license_plate
@@ -156,7 +175,7 @@ def apply_ocr_v2(_license_plate_region):
     whitelist = set("ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789")
     try:
         # Apply OCR to recognize characters in the license plate region
-        ocr_text = pytesseract.image_to_string(_license_plate_region, config='--psm 8')
+        ocr_text = pytesseract.image_to_string(_license_plate_region, config='--psm 7')
 
         # Filter OCR result based on the whitelist
         filtered_text = ''.join(char for char in ocr_text if char.upper() in whitelist)
@@ -184,7 +203,7 @@ def post_process_ocr(ocr_text):
         return None
 
 
-def annotate_image(original_image, license_plate_contour, recognized_text):
+def annotate_image(f_name, original_image, license_plate_contour, recognized_text):
     try:
         # Draw bounding box around the license plate region
         x, y, w, h = cv2.boundingRect(license_plate_contour)
@@ -193,53 +212,39 @@ def annotate_image(original_image, license_plate_contour, recognized_text):
         # Overlay recognized text on the annotated image
         cv2.putText(annotated_image, recognized_text, (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 255, 0), 2)
 
-        # Display the annotated image (optional)
-        cv2.imshow("Annotated Image", annotated_image)
-        cv2.waitKey(0)
-        cv2.destroyAllWindows()
+        if SHOW_IMAGES:
+            # Display the annotated image (optional)
+            cv2.imshow("Annotated Image", annotated_image)
+            cv2.waitKey(0)
+            cv2.destroyAllWindows()
 
         # Save the annotated image to a specified folder location (optional)
-        cv2.imwrite("annotated_image.jpg", annotated_image)
+        cv2.imwrite(f"OUTPUT/{f_name}_annotated_image.jpg", annotated_image)
         return [x, y, w, h]
     except Exception:
         logging.exception("ANNOTATION FAILURE", exc_info=True)
         return None
 
 
-def save_annotated_image(annotated_image, output_folder, file_name, annotation_data):
-    try:
-        # Ensure the output folder exists
-        if not os.path.exists(output_folder):
-            os.makedirs(output_folder)
-
-        # Generate the file path for saving the annotated image
-        output_path = os.path.join(output_folder, file_name)
-
-        # Save the annotated image to the specified folder location
-        cv2.imwrite(output_path, annotated_image)
-
-        logging.info(f"Annotated image saved successfully to: {output_path}")
-
-        # Save annotation information in XML format
-        save_annotation_xml(output_folder, file_name, annotation_data)
-    except Exception:
-        logging.exception("FAILED TO SAVE ANNOTATED IMAGE", exc_info=True)
-
-
-def save_annotation_xml(output_folder, file_name, annotation_data):
+def save_annotation_xml(OUTPUT_FOLDER, file_name, annotation_data):
     try:
         # Generate the XML file path
-        xml_file_path = os.path.join(output_folder, f"{file_name.split('.')[0]}.xml")
+        xml_file_path = os.path.join(OUTPUT_FOLDER, f"{file_name.split('.')[0]}.xml")
 
         # Create the XML structure
         root = ET.Element("annotation")
         object_elem = ET.SubElement(root, "object")
-        name_elem = ET.SubElement(object_elem, "name")
-        name_elem.text = annotation_data["name"]  # Example: License plate number
-        bbox_elem = ET.SubElement(object_elem, "bndbox")
-        xmin_elem = ET.SubElement(bbox_elem, "xmin")
-        xmin_elem.text = str(annotation_data["xmin"])
-        # Add other bounding box coordinates (ymin, xmax, ymax) as needed
+        for key, value in annotation_data.items():
+            if key == "name":
+                name_elem = ET.SubElement(object_elem, key)
+                name_elem.text = value
+            elif key == "license_plate":
+                license_plate_elem = ET.SubElement(object_elem, key)
+                license_plate_elem.text = value
+            elif key in ("xmin", "ymin", "width", "height"):
+                bbox_elem = ET.SubElement(object_elem, "bndbox")
+                coord_elem = ET.SubElement(bbox_elem, key)
+                coord_elem.text = str(value)
 
         # Create the XML tree and write to the XML file
         tree = ET.ElementTree(root)
@@ -251,44 +256,57 @@ def save_annotation_xml(output_folder, file_name, annotation_data):
 
 
 if __name__ == "__main__":
-    img = load_image()
-    preprocessed_img = preprocess_image(
-        _image=img
-    )
-    contrs = detect_and_localize(
-        _preproc_image=preprocessed_img
-    )
-    lp_contour = extract_lp_region(
-        _contours=contrs,
-        _preprocessed_image=preprocessed_img
-    )
-    segmented_lp = further_processing(
-        _license_plate_contour=lp_contour,
-        _preprocessed_image=preprocessed_img
-    )
-    ocr_result = apply_ocr_v2(
-        _license_plate_region=segmented_lp
-    )
-    filtered_result = post_process_ocr(
-        ocr_text=ocr_result
-    )
-    bound_box = annotate_image(
-        original_image=img,
-        license_plate_contour=lp_contour,
-        recognized_text=filtered_result
-    )
-    annotation_data = {
-        "name": "example_file.jpg",  # Example: File name
-        "license_plate": "MH20EE7598",  # Example: License plate number
-        "xmin": 100,  # Example: Bounding box xmin
-        "xmin": 100,  # Example: Bounding box xmin
-        "xmin": 100,  # Example: Bounding box xmin
-        "xmin": 100,  # Example: Bounding box xmin
-    }
+    file_list = os.listdir(INPUT_IMAGES)
+    print(file_list)
+    for f in file_list:
+        stripped_name = f.split(".png")[0]
 
-    annotated_image = cv2.imread("C:\\Users\\Brandon\\personal_code\\CARL\\annotated_image.jpg")
-    output_folder = "C:\\Users\\Brandon\\personal_code\\CARL\\OUTPUT\\"  # Specify the output folder path
-    file_name = "annotated_result.jpg"  # Specify the file name for the annotated image
-    save_annotated_image(annotated_image, output_folder, file_name)  # Save the annotated image
+        img = load_image(
+            file2load=f
+        )
+        preprocessed_img = preprocess_image(
+            _image=img
+        )
+        contrs = detect_and_localize(
+            _preproc_image=preprocessed_img
+        )
+
+        # to help with debugging
+        # epsilon_values = [0.07, 0.08, 0.09]  # Adjust epsilon values to experiment
+        # for epsilon in epsilon_values:
+        #     lp_contour = extract_lp_region(contrs, preprocessed_img, epsilon=epsilon)
+        # break
+
+        lp_contour = extract_lp_region(
+            _contours=contrs,
+            _preprocessed_image=preprocessed_img
+        )
+        segmented_lp = further_processing(
+            _license_plate_contour=lp_contour,
+            _preprocessed_image=preprocessed_img
+        )
+        ocr_result = apply_ocr_v2(
+            _license_plate_region=segmented_lp
+        )
+        filtered_result = post_process_ocr(
+            ocr_text=ocr_result
+        )
+        bound_box = annotate_image(
+            f_name=stripped_name,
+            original_image=img,
+            license_plate_contour=lp_contour,
+            recognized_text=filtered_result
+        )
+        annotation_data = {
+            "name": "Cars111.png",
+            "license_plate": filtered_result,
+            "xmin": bound_box[0],
+            "ymin": bound_box[1],
+            "width": bound_box[2],
+            "height": bound_box[3],
+        }
+
+        file_name = f"{stripped_name}_annotated_result.jpg"
+        save_annotation_xml(OUTPUT_FOLDER, file_name, annotation_data)
 
 
